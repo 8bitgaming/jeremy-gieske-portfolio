d2026c87464435cd23425d891199a590d0c65f66
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-04-19 01:25:35 -0500

    starting to add menu

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function About() {
+  return(
+      <>
+    <div>About Me</div>
+    <p>Passionate about technology and especially agile development, I currently work in Target Technology Services as a Lead Scrum Master supporting corporate systems. My current team is using React to develop custom apps for our store and distribution team members.</p>
+    </>
+  )
+  
+};
+
+export default About
